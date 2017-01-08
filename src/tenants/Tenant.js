e9b843aff31e876a9f52d88db005d9962f8a999c
jear-jujaroen <jear.jujaroen@gmail.com>
2017-01-08 13:49:54 -0500

    feat(): Add table to TenantList
    add a table to render a list of tenants

--- src/tenants/Tenant.js
+++ src/tenants/Tenant.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TenantTable from './TenantList';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const Tenant = () => (
-  <div>
-    <h1>Tenants</h1>
-  </div>
+  <MuiThemeProvider>
+    <TenantTable />
+  </MuiThemeProvider>
 );
 
 export default Tenant;
