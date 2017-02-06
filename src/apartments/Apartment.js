a677d0947224edc7d114b41c265891c8defad6ea
jear-jujaroen <jear.jujaroen@gmail.com>
2017-02-06 01:06:51 -0500

    feat: Make apartment component render two tabs
    First tab shows apartment list and second tab is for a form

--- src/apartments/Apartment.js
+++ src/apartments/Apartment.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ApartmentTable from './ApartmentListContainer'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import {Tabs, Tab} from 'material-ui/Tabs'
+// import muiTheme from 'material-ui/styles/muiTheme'
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400
+  }
+}
 
 const Apartment = () => (
+
   <MuiThemeProvider>
-    <ApartmentTable />
+    <Tabs
+      initialSelectedIndex={0}
+    >
+      <Tab label='Item One' >
+        <div>
+          <h2 style={styles.headline}>Apartment List</h2>
+          <ApartmentTable />
+        </div>
+      </Tab>
+      <Tab label='Item Two' >
+        <div>
+          <h2 style={styles.headline}>Apartment Form</h2>
+          <p>
+            This is another example tab.
+          </p>
+        </div>
+      </Tab>
+    </Tabs>
   </MuiThemeProvider>
 )
 
