9a680cc272cdcd8af956f91a3fb331a3b94049f6
jear-jujaroen <jear.jujaroen@gmail.com>
2017-02-06 16:11:51 -0500

    refactor: Clean up code

--- src/apartments/Apartment.js
+++ src/apartments/Apartment.js
@@ -20,7 +20,6 @@ const style = {
   margin: 20,
   textAlign: 'center',
   display: 'inline-block'
-  // alignSelf: 'center'
 }
 
 const Apartment = () => (
@@ -51,14 +50,3 @@ const Apartment = () => (
 )
 
 export default Apartment
-
-// <CardText>
-//   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-//   Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-//   Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-//   Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-// </CardText>
-// <CardActions>
-//   <FlatButton label='Action1' />
-//   <FlatButton label='Action2' />
-// </CardActions>
