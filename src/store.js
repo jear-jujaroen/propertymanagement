576f2b00f49ef96b46f3606c6dd727fd12e4dda1
jear-jujaroen <jear.jujaroen@gmail.com>
2017-02-19 16:16:38 -0500

    perf(): Attempt to install redux dev tools
    Not breaking but also not working

--- src/store.js
+++ src/store.js
@@ -2,7 +2,12 @@ import { createStore } from 'redux'
 
 import rootReducer from './reducers/reducers'
 
-const store = createStore(rootReducer)
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+/* eslint-enable */
 
 store.subscribe(() => {
   console.log('store changed ++> ', store.getState())
