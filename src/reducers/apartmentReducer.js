a27cce3a3e6fbb367e08082ca6bb9bd5ad2a3982
jear-jujaroen <jear.jujaroen@gmail.com>
2017-02-19 18:53:54 -0500

    feat(  ): Add actionTypes
    Currently not working

--- src/reducers/apartmentReducer.js
+++ src/reducers/apartmentReducer.js
@@ -1,7 +1,30 @@
-export default function () {
-  return [
-    {name: 'Apartment #1', status: '0'},
-    {name: 'Apartment #2', status: '1'},
-    {name: 'Apartment #3', status: '3'}
-  ]
+import * as ApartmentActionTypes from '../actions/apartment/actionTypes'
+
+// export default function () {
+//   return [
+//     {name: 'Apartment #1', status: '0'},
+//     {name: 'Apartment #2', status: '1'},
+//     {name: 'Apartment #3', status: '3'}
+//   ]
+// }
+
+export default function Apartment (state = [], action) {
+  switch (action.type) {
+    case ApartmentActionTypes.ADD_APARTMENT:
+      return [
+        ...state,
+        {
+          address: action.address,
+          status: action.status
+        }
+      ]
+
+    case ApartmentActionTypes.REMOVE_APARTMENT:
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ]
+    default:
+      return state
+  }
 }
