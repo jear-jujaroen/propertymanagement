035593556f8f04c5c1e83b445f067a21ea6c6259
jear-jujaroen <jear.jujaroen@gmail.com>
2017-02-25 17:42:19 -0500

    chore(): Create a server and add middleware
    Have not hooked up the routes yet

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 'use strict'
 
 const express = require('express')
-
-const PORT = 8000
-
 const app = express()
-app.get('/', function (req, res) {
-  res.send('Welcome to PropertyManagement')
+const bodyParser = require('body-parser')
+
+// to make parsing requests easier //
+// this middleware allows us to parse the body as json and make it accessible from req.body property
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use('/', function (req, res, next) {
+  console.log('testing req.body is', req.body.color)
+  next()
 })
 
-app.listen(PORT)
-console.log('running on http://localhost:' + PORT)
+const port = process.env.PORT || 3000
+
+app.listen(port, function () {
+  console.log('running on http://localhost:' + port)
+})
