f96a7a9ec67202cd625168f96be325a4a6a47ae6
jear-jujaroen <jear.jujaroen@gmail.com>
2017-02-26 20:51:47 -0500

    feat( ): Add apartment routes and connect it to the server
    Currently not working

--- server/server.js
+++ server/server.js
@@ -10,21 +10,30 @@ const morgan = require('morgan')
 // give us colorful status codes for our API responses
 app.use(morgan('dev'))
 
-// to make parsing requests easier ///
+// to make parsing requests easier //
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-const router = express.Router()
-
+// looks for index.html in public folder
 app.use(express.static(path.join(__dirname, '../public')))
 
-const port = process.env.PORT || 3030
+// ROUTES //
+const router = express.Router()
 
-// app.get('/', function (req, res) {
-//   res.send('Welcome to PropertyManagement')
-// })
+// middleware for every request //
+router.use((req, res, next) => {
+  console.log('Every request goes through here!')
+  next()
+})
+
+// modularized routes //
+const apartmentRoutes = require('./routes/apartmentRoutes')
+
+app.use('/apartments', apartmentRoutes)
+
+const port = process.env.PORT || 3030
 
 app.listen(port)
 console.log('running on http://localhost:' + port)
